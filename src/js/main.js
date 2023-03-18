88c11d23bbd60cdc9c5e5f36205d2ef107905395
ChurrinChurron <marianafantasy8@gmail.com>
2023-03-18 16:18:53 -0400

    cornejo

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,10 @@ let botonCerrarNav = document.querySelector("#close-menu");
 let menuNav = document.querySelector(".menu__interno");
 let listaAreas = document.querySelector("#areas");
 let linkAreas = document.querySelector("#linkAreasMovil");
+let menuVideos = document.querySelector("#menuCateg");
+let listaCateg = document.querySelector("#categAreas");
+let video = document.querySelector("#video");
+let fondoVideo = document.querySelector(".video__filtro");
 
 /*FUNCION ABRIR Y CERRAR MENU NAV*/
 
@@ -21,4 +25,39 @@ botonCerrarNav.addEventListener("click", () => {
 listaAreas.addEventListener("click", () => {
 
     linkAreas.classList.toggle("cerrar");
+})
+
+/*FUNCION ABRIR MENU CATEGORIA VIDEOS*/
+
+menuVideos.addEventListener("click", () => {
+
+    listaCateg.classList.toggle("ocultar");
+})
+    
+if(window.innerWidth >= 992) {
+    
+    listaCateg.classList.remove("ocultar");
+
+    menuVideos.addEventListener("click", () => {
+
+        listaCateg.classList.remove("ocultar");
+    })
+}
+
+/*FUNCION REPRODUCIR Y PAUSAR VIDEO*/
+
+video.addEventListener("click", () => {
+    
+    if(video.paused) {
+
+        video.play();
+    
+        fondoVideo.classList.add("play");
+
+    } else {
+
+        video.pause();
+        
+        fondoVideo.classList.remove("play");
+    }
 })
